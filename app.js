6eb78987ef7e60e1c0e7feb5ddd0cfaa6db9563b
cmullaney <cpmullaney94@gmail.com>
2018-06-16 13:47:27 -0700

    converted to express app

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require("express"),
+	app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+// Routes
+
+app.get("/", function(req, res) {
+	res.redirect("/home");
+});
+
+app.get("/home", function(req, res) {
+	res.render("index");
+});
+
+app.get("/about", function(req, res) {
+	res.render("about");
+});
+
+app.get("/resume", function(req, res) {
+	res.render("resume");
+});
+
+app.get("/gallery", function(req, res) {
+	res.render("gallery");
+
+});
+app.get("/contact", function(req, res) {
+	res.render("contact");
+});
+
+app.listen(3000, "127.0.0.1", function() {
+    console.log("App is listening...");
+});
